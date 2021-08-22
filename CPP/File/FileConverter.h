f7ab7919f6232225e701bdad366fbaddc4309965
szlobipeti <szlobipeti@gmail.com>
2021-08-22 22:14:52 +0200

    Updated FileConverter.h

--- CPP/File/FileConverter.h
+++ CPP/File/FileConverter.h
@@ -8,14 +8,14 @@ namespace gen
 	{
 		class FileConverter
 		{
-		private: gen::file::File* File1 = nullptr;
-		private: gen::file::File* File2 = nullptr;
+		protected: gen::file::File* File1 = nullptr;
+		protected: gen::file::File* File2 = nullptr;
 
 		public: FileConverter();
 		public: FileConverter(gen::file::File* File1, gen::file::File* File2);
 
-		protected: virtual void vSetFile1(gen::file::File* File1);
-		protected: virtual void vSetFile2(gen::file::File* File2);
+		protected: virtual bool oSetFile1() = 0;
+		protected: virtual bool oSetFile2() = 0;
 		protected: virtual bool oConvertFrom1To2() = 0;
 		protected: virtual bool oConvertFrom2To1() = 0;
 		};
