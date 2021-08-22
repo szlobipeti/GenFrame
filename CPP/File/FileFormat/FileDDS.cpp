e8dd141258ce605a12a55fe722b8a4d25c9f50cb
szlobipeti <szlobipeti@gmail.com>
2021-08-22 21:16:44 +0200

    re-added FileDDS.cpp

---
+++ CPP/File/FileFormat/FileDDS.cpp
@@ -0,0 +1,45 @@
+#include "FileDDS.h"
+
+gen::file::format::FileDDS::~FileDDS()
+{
+	if (PixelData != nullptr)
+	{
+		free(PixelData);
+	}
+	return;
+}
+
+bool gen::file::format::FileDDS::oRead(std::ifstream& inFile, size_t dataBegin, size_t dataSize)
+{
+	if (dataSize < 128)
+	{
+		return false;
+	}
+
+	uint32_t Magic = 0;
+	inFile.read(reinterpret_cast<char*>(&Magic), 4);
+
+	if (Magic != DDS_MAGIC)
+	{
+		return false;
+	}
+
+	inFile.read(reinterpret_cast<char* (&header), 124);
+
+	PixelDataSize = dataSize - 128;
+	PixelData = (char*)malloc(PixelDataSize);
+	inFile.read(PixelData, PixelDataSize);
+
+	return true;
+}
+
+bool gen::file::format::FileDDS::oWrite(std::ofstream& outFile)
+{
+	uint32_t Magic = DDS_MAGIC;
+	outFile.write(reinterpret_cast<char*>(&Magic), 4);
+
+	outFile.write(reinterpret_cast<char*>(&Header), 124);
+	outFile.write(PixelData, PixelDataSize);
+
+	return true;
+}
