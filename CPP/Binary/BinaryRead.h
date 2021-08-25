06cadb347ec4c7629fd8922b1d26aeca50509a70
TO-BE.MEDIA Allgemein <office@to-be.media>
2021-08-25 17:14:17 +0200

    Implemented binary function for reading little or big endian integers

--- CPP/Binary/BinaryRead.h
+++ CPP/Binary/BinaryRead.h
@@ -10,80 +10,82 @@ namespace gen
 	{
 		namespace read
 		{
+			// Read to Value from inFile for the specified size
+			template<typename T>
+			__forceinline typename std::enable_if<std::is_pointer<T>::value, void>::type Read(std::ifstream& inFile, T Value, size_t Size)
+			{
+				inFile.read(reinterpret_cast<char*>(Value), Size);
+			}
+
+			// Read an unsigned integer of specified Size for block size, then read the following block to Block. Block has to be a nullptr to be able to read. Size is capped to 8
+			template<typename T>
+			__forceinline typename std::enable_if<std::is_pointer<T>::value, bool>::type ReadFixedBlock(std::ifstream& inFile, T Block, size_t Size)
+			{
+				if (Size == 0)
+				{
+					return false;
+				}
+
+				if (Block != nullptr)
+				{
+					return false;
+				}
+
+				if (Size > 8)
+				{
+					Size = 8;
+				}
+
+				uint64_t Count = 0;
+
+				Read(inFile, &Count, Size);
+				Block = (T)malloc(Count);
+				Read(inFile, Block, Count);
+
+				return true;
+			}
+
+			// Read an unsigned integer of specified Size for String legnth, then read the String. Size is capped to size of size_t
+			__forceinline bool ReadString(std::ifstream& inFile, std::string String, size_t Size)
+			{
+				if (Size == 0)
+				{
+					return false;
+				}
+
+				if (Size > sizeof(size_t))
+				{
+					Size = sizeof(size_t);
+				}
+
+				size_t Count = 0;
+
+				Read(inFile, &Count, Size);
+				String.resize(Count);
+				Read(inFile, &String[0], Count);
+
+				return true;
+			}
+
 			namespace little
 			{
-				template<typename T>
-				__forceinline typename std::enable_if<std::is_pointer<T>::value, void>::type Read(std::ifstream& inFile, T Value, size_t Size)
+				template<typename U>
+				__forceinline typename std::enable_if<(!std::is_pointer<U>::value && std::is_integral<U>::value), void>::type Read(std::ifstream& inFile, U Value)
 				{
-					inFile.read(reinterpret_cast<char*>(Value), Size);
-				}
+					inFile.read(reinterpret_cast<char*>(&Value), sizeof(U));
+				}		
+			}
 
-				template<typename T>
-				__forceinline typename std::enable_if<std::is_pointer<T>::value, bool>::type ReadBlock(std::ifstream& inFile, T Block, size_t Size)
+			namespace big
+			{
+				template<typename U>
+				__forceinline typename std::enable_if<(!std::is_pointer<U>::value&& std::is_integral<U>::value), void>::type Read(std::ifstream& inFile, U Value)
 				{
-					if (Size == 0)
+					for (size_t i = 0; i < sizeof(U); i++)
 					{
-						return false;
+						gen::binary::read::Read(inFile, (char*)(&Value) + (sizeof(U) - i - 1), 1);
 					}
-
-					if (Block != nullptr)
-					{
-						return false;
-					}
-
-					if (Size > 8)
-					{
-						Size = 8;
-					}
-
-					uint64_t Count = 0;
-
-					Read(inFile, &Count, Size);
-					Block = (T)malloc(Count);
-					Read(inFile, Block, Count);
-
-					return true;
 				}
-
-				__forceinline bool ReadString(std::ifstream& inFile, std::string String, size_t Size)
-				{
-					if (Size == 0)
-					{
-						return false;
-					}
-
-					if (Size > sizeof(size_t))
-					{
-						Size = sizeof(size_t);
-					}
-
-					size_t Count = 0;
-
-
-
-					Read(inFile, &Count, Size);
-					Read(inFile, &Count, Size);
-					String.resize(Count);
-					Read(inFile, &String[0], Count);
-
-					return true;
-				}
-
-				/*
-				// Default Templates
-				
-				// Read to Value from inFile for the specified size
-				template <typename T>
-				extern inline typename std::enable_if<std::is_pointer<T>::value, void>::type Read(std::ifstream& inFile, T Value, size_t Size);
-
-				// Read an unsigned integer of specified Size for block size, then read the following block to Block. Block has to be a nullptr to be able to read. Size is capped to 8
-				template <typename T>
-				extern inline typename std::enable_if<std::is_pointer<T>::value, bool>::type ReadBlock(std::ifstream& inFile, T Value, size_t Size);
-
-				// Read an unsigned integer of specified Size for String legnth, then read the String. Size is capped to size of size_t
-				extern inline bool ReadString(std::ifstream& inFile, std::string String, size_t Size);
-				*/
-
 			}
 		}
 	}
--- CPP/Binary/BinaryWrite.h
+++ CPP/Binary/BinaryWrite.h
@@ -2,3 +2,72 @@
 
 #include <fstream>
 #include <stdint.h>
+#include <string>
+
+namespace gen
+{
+	namespace binary
+	{
+		namespace write
+		{
+			template<typename T>
+			__forceinline typename std::enable_if<std::is_pointer<T>::value, void>::type Write(std::ofstream& outFile, T Value, size_t Size)
+			{
+				outFile.write(reinterpret_cast<char*>(Value), Size);
+			}
+
+			template<typename T, typename U>
+			__forceinline typename std::enable_if<(std::is_pointer<T>::value&& std::is_unsigned<U>::value), bool>::type WriteFixedBlock(std::ofstream& outFile, T Block, U Size)
+			{
+				if (Block != nullptr)
+				{
+					return false;
+				}
+
+				Write(outFile, &Size, sizeof(U));
+				Write(outFile, Block, Size);
+
+				return true;
+			}
+
+			__forceinline bool WriteString(std::ofstream& outFile, std::string String, size_t Size = 0)
+			{
+				if (String.length() == 0)
+				{
+					return false;
+				}
+
+				if (Size == 0 || Size > String.length())
+				{
+					Size = String.length();
+				}
+
+				Write(outFile, &Size, sizeof(size_t));
+				Write(outFile, &String[0], Size);
+
+				return true;
+			}
+
+			namespace little
+			{
+				template<typename U>
+				__forceinline typename std::enable_if<(!std::is_pointer<U>::value&& std::is_integral<U>::value), void>::type Write(std::ofstream& outFile, U Value)
+				{
+					outFile.write(reinterpret_cast<char*>(&Value), sizeof(U));
+				}
+			}
+
+			namespace big
+			{
+				template<typename U>
+				__forceinline typename std::enable_if<(!std::is_pointer<U>::value&& std::is_integral<U>::value), void>::type Write(std::ofstream& outFile, U Value)
+				{
+					for (size_t i = 0; i < sizeof(U); i++)
+					{
+						gen::binary::write::Write(outFile, (char*)(&Value) + (sizeof(U) - i - 1), 1);
+					}
+				}
+			}
+		}
+	}
+}
