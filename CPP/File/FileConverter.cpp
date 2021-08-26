d0d181bd455eda7a73e3b49b92172bc642d95e67
TO-BE.MEDIA Allgemein <office@to-be.media>
2021-08-26 14:52:20 +0200

    Reworking FileConverter, adding template and virtual functions

--- CPP/File/FileConverter.cpp
+++ CPP/File/FileConverter.cpp
@@ -1,12 +1,1 @@
 #include "FileConverter.h"
-
-gen::file::FileConverter::FileConverter()
-{
-	return;
-}
-
-gen::file::FileConverter::FileConverter(gen::file::File* File1, gen::file::File* File2)
-{
-	this->File1 = File1;
-	this->File2 = File2;
-}
--- CPP/File/FileConverter.h
+++ CPP/File/FileConverter.h
@@ -6,16 +6,64 @@ namespace gen
 {
 	namespace file
 	{
-		class FileConverter
+		namespace convert
 		{
-		protected: gen::file::File* File1 = nullptr;
-		protected: gen::file::File* File2 = nullptr;
+			template <typename T, typename = typename std::enable_if<std::is_base_of<gen::file::File, T>::value>::type>
+			class FileConverter
+			{
+			protected: T* Inner = nullptr;
+			protected: gen::file::File* Outer = nullptr;
 
-		public: FileConverter();
-		public: FileConverter(gen::file::File* File1, gen::file::File* File2);
+			public: FileConverter() = default;
+			public: FileConverter(T* Inner, gen::file::File* Outer)
+			{
+				this->Inner = Inner;
+				this->Outer = Outer;
+			};
 
-		protected: virtual bool oConvertFrom1To2() = 0;
-		protected: virtual bool oConvertFrom2To1() = 0;
-		};
+			public: __forceinline void SetInner(T* Inner) { this->Inner = Inner; };
+			public: __forceinline void SetOuter(gen::file::File* Outer) { this->Outer = Outer; };
+
+			public: __forceinline bool ConvertFromInnerToOuter()
+			{
+				if (Inner == nullptr || Outer == nullptr)
+				{
+					return false;
+				}
+
+				return oConvertFromInnerToOuter();
+			};
+			public: __forceinline bool ConvertFromOuterToInner()
+			{
+				if (Inner == nullptr || Outer == nullptr)
+				{
+					return false;
+				}
+
+				return oConvertFromOuterToInner();
+			};
+
+			protected: virtual bool oConvertFromInnerToOuter() = 0;
+			protected: virtual bool oConvertFromOuterToInner() = 0;
+			};
+
+			template<typename T, typename I>
+			typename std::enable_if<std::is_base_of<FileConverter<I>, T>::value && !std::is_abstract<T>::value, bool>::type ConvertFromInnerToOuter(I* Inner, gen::file::File* Outer)
+			{
+				T converter;
+				converter.SetInner(Inner);
+				converter.SetOuter(Outer);
+				return converter.ConvertFromInnerToOuter();
+			}
+
+			template<typename T, typename O>
+			typename std::enable_if<std::is_base_of<FileConverter<T>, T>::value && !std::is_abstract<T>::value, bool>::type ConvertFromOuterToInner(O* Outer, gen::file::File* Inner)
+			{
+				T converter;
+				converter.SetOuter(Outer);
+				converter.SetInner(Inner);
+				return converter.ConvertFromOuterToInner();
+			}
+		}
 	}
 }
